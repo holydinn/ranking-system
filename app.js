c6622425c168932c8b65359d21eca2117341cc2d
holydinn <diana.imamieva@yandex.ru>
2022-10-19 15:10:35 +0300

    ranking-done

--- app.js
+++ app.js
@@ -124,9 +124,6 @@ function relativeMajorityRule(rankedMatrix) {
 function condorcetWinner(rankedMatrix) {
   let altsPoints = Array(participants.length).fill(0)  //очки альтернативы в попарном сравнении
 
-  //транспонированная матрица Эксперт-Ранг для удобного обхода ранжировки
-  rankedMatrix = transposeMatrix(rankedMatrix)
-
   //матрица сравнений
   let comparisonMatrix = createCompareMatrix(rankedMatrix)
 
@@ -154,9 +151,6 @@ function copelandRule(rankedMatrix) {
   let altsPoints = Array(participants.length).fill(0)  //очки альтернативы в попарном сравнении
   resRank = []
 
-  //транспонированная матрица Эксперт-Ранг для удобного обхода ранжировки
-  rankedMatrix = transposeMatrix(rankedMatrix)
-
   //матрица сравнений
   let comparisonMatrix = createCompareMatrix(rankedMatrix)
 
@@ -183,9 +177,6 @@ function simpsonRule(rankedMatrix) {
   let temp = []
   resRank = []
 
-  //транспонированная матрица Эксперт-Ранг для удобного обхода ранжировки
-  rankedMatrix = transposeMatrix(rankedMatrix)
-
   //матрица сравнений
   let comparisonMatrix = createCompareMatrix(rankedMatrix)
 
@@ -205,9 +196,6 @@ function bordRule(rankedMatrix) {
   let altsPoints = Array(participants.length).fill(0)  //очки альтернатив в сравнении
   resRank = []
 
-  //транспонированная матрица Эксперт-Ранг для удобного обхода ранжировки
-  rankedMatrix = transposeMatrix(rankedMatrix)
-
   rankedMatrix.forEach(exp => {
     exp.forEach((alt, altIndex) => {
       altsPoints[alt - 1] += exp.length - (altIndex - 1)
@@ -220,20 +208,202 @@ function bordRule(rankedMatrix) {
 
 }
 
+//создание матрицы отношений
+function createRelationalMatrix(matrix) {
+  let expertRank = matrix.slice()
+
+  let relMatrix = Array(participants.length).fill(0).map(() => Array(participants.length).fill(0))
+  let preferredItems = []  //массив предпочтительных альтернатив
+
+  do {
+    //удаляем предпочтительную альтернативу из дальнейшей обработки
+    preferredItems.unshift(expertRank.shift() - 1)
+    relMatrix[preferredItems[0]].forEach((value, index, array) => {
+
+      // 1 - если альт-вы нет в preferredItems, т.е. она лучше
+      // -1 - если альт-ва есть в preferredItems, т.е. она хуже
+      if (!preferredItems.includes(index)) {
+        array[index] = 1
+      } else {
+        array[index] = -1
+      }
+      array[preferredItems[0]] = 0
+    })
+  } while (expertRank.length > 0)
+  preferredItems.length = 0
+  return relMatrix
+}
+
+//поиск растояния между ранжировками по Кемени
+function findDistance(matrix1, matrix2) {
+  let dist = 0
+  let i, j
+
+  for (i = 0; i < matrix1.length - 1; i++) {
+    for (j = i + 1; j < matrix1.length; j++) {
+      dist += Math.abs((matrix1[i][j] - matrix2[i][j]))
+    }
+  }
+
+  return dist
+}
+
+//Матрица расстояний для ранжировок по Кемени
+function createDistanceMatrix(matrixArray) {
+  let distMatrix = Array(matrixArray.length).fill(0).map(() => Array(matrixArray.length).fill(0))
+  let curMatrix, nextMatrix
+
+  for (curMatrix = 0; curMatrix < matrixArray.length - 1; curMatrix++) {
+    for (nextMatrix = curMatrix + 1; nextMatrix < matrixArray.length; nextMatrix++) {
+      distMatrix[curMatrix][nextMatrix] = distMatrix[nextMatrix][curMatrix] = findDistance(matrixArray[curMatrix], matrixArray[nextMatrix])
+    }
+  }
+  return distMatrix
+}
+
+//Поиск индекса минимальной суммы ранжировки
+function findMinDist(distMatrix) {
+
+  let sumDist = []  //сумма растояний
+
+  distMatrix.forEach((elem, index) => {
+    sumDist[index] = elem.reduce((acc, val) => acc + val, 0)
+  })
+
+  let minDist
+  if (!sumDist.includes(0)) {
+    minDist = Math.min.apply(null, sumDist)
+  } else {
+    let temp = sumDist.filter((val) => {
+      return val !== 0
+    })
+    minDist = Math.min.apply(null, temp)
+  }
+
+  return sumDist.indexOf(minDist)
+}
+
+//Медиана Кемени
+function medianKemeny(rankedMatrix) {
+
+  resRank = []
+  let relMatrices = []  //массив матриц отношений
+
+  rankedMatrix.forEach((exp, expIndex) => {
+    relMatrices[expIndex] = createRelationalMatrix(exp)
+  })
+
+  //матрица расстояний
+  let distanceMatrix = createDistanceMatrix(relMatrices)
+
+  const indexMinDist = findMinDist(distanceMatrix)
+
+  resRank = rankedMatrix[indexMinDist]
+
+  return {name: 'Медиана Кемени', resRank}
+}
+
+//Матрица потерь для новой медианы Кемени
+function createLossMatrix(matrixArray) {
+  let i, j
+  let lossMatrix = Array(participants.length).fill(0).map(() => Array(participants.length).fill(0))
+
+  matrixArray.forEach(matrix => {
+    for (i = 0; i < matrix.length; i++) {
+      for (j = 0; j < matrix.length; j++) {
+        switch (matrix[i][j]) {
+          case 0:
+            lossMatrix[i][j] += 1
+            break
+          case -1:
+            lossMatrix[i][j] += 2
+            break
+        }
+      }
+    }
+  })
+
+  return lossMatrix
+}
+
+//Медиана Кемени в виде новой ранжировки
+function newMedianKemeny(rankedMatrix) {
+  resRank = []
+  let relMatrices = []  //массив матриц отношений
+
+  //построение матриц отношений
+  rankedMatrix.forEach((exp, expIndex) => {
+    relMatrices[expIndex] = createRelationalMatrix(exp)
+  })
+
+  //создание матрицы потерь
+  let lossMatrix = createLossMatrix(relMatrices)
+
+  let indexArray = []
+  for (let i = 0; i < lossMatrix.length; i++) {
+    indexArray[i] = i
+  }
+
+  let altIndex
+  let tempLossMatrix = lossMatrix.slice()
+
+  do {
+    altIndex = findMinDist(tempLossMatrix)
+    resRank.push(altIndex + 1)
+    indexArray.splice(indexArray.indexOf(altIndex), 1)
+    tempLossMatrix = deleteMatrixRowClm(tempLossMatrix, altIndex)
+
+  } while (indexArray.length > 0)
+
+  return {name: 'Медиана Кемени в виде новой ранжировки', resRank}
+}
+
+function deleteMatrixRowClm(matrix, altIndex) {
+
+  matrix[altIndex].forEach((item, index) => {
+    matrix[altIndex][index] = 0
+  })
+
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i][altIndex] = 0
+  }
+  return matrix
+
+}
+
 const expertRangMatrix = createMatrixExpertRang()
 //console.log(expertRangMatrix)
 
+const transRankedMatrix = transposeMatrix(expertRangMatrix)  //транспонированная матрица Эксперт-Ранг для удобного обхода ранжировки
+// const resRankings=[]
+// resRankings.push(relativeMajorityRule(expertRangMatrix))
+// resRankings.push(condorcetWinner(transRankedMatrix))
+// resRankings.push(copelandRule(transRankedMatrix))
+// resRankings.push(simpsonRule(transRankedMatrix))
+// resRankings.push(bordRule(transRankedMatrix))
+// console.log(resRankings[0].resRank)
+
+const start = new Date().getTime();
 const resMajorityRule = relativeMajorityRule(expertRangMatrix)
 console.log(resMajorityRule)
 
-const resCondorcetWinner = condorcetWinner(expertRangMatrix)
+const resCondorcetWinner = condorcetWinner(transRankedMatrix)
 console.log(resCondorcetWinner)
 
-const resCopelandRule = copelandRule(expertRangMatrix)
+const resCopelandRule = copelandRule(transRankedMatrix)
 console.log(resCopelandRule)
 
-const resSimpsonRule = simpsonRule(expertRangMatrix)
+const resSimpsonRule = simpsonRule(transRankedMatrix)
 console.log(resSimpsonRule)
 
-const resBordRule = bordRule(expertRangMatrix)
+const resBordRule = bordRule(transRankedMatrix)
 console.log(resBordRule)
+
+const resMedianKemeny = medianKemeny(transRankedMatrix)
+console.log(resMedianKemeny)
+
+const resNewMedianKemeny = newMedianKemeny(transRankedMatrix)
+console.log(resNewMedianKemeny)
+
+const end = new Date().getTime();
+console.log(`SecondWay: ${end - start}ms`);
